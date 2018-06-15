3dd4d247a6ef0146be1988382349b42955615ea1
Abayomi Akinsola <olasunkami010@gmail.com>
2018-06-15 20:57:42 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,25 @@
 // declare your functions here...
+//$(document).ready(function(){
+   
+  function paragraphSelector(){
+  return $('div p').$('*');
+ 
+}
+
+function lastImageSelector(){
+ return $('div img:last').$(':image')
+}
+
+function ninjaBabySelector(){
+  return $('#baby-ninja img')
+}
+
+function divSelector(){
+  return $('.pics')
+}
+
+function firstListItem(){
+  return $('#pic-list ul li')
+}
+//});
+
